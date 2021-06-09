8d248bd133320009270915678ed06fe376b6a3ac
Evan Schoening <evan.schoening@gmail.com>
2021-06-09 04:46:00 -0700

    added functions for add sub

---
+++ calculator.py
@@ -0,0 +1,9 @@
+'''
+calculator for basic math operations
+'''
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
